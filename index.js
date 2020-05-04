afd54de31d7402ec510bed645d2f0a8ef74be577
hyunmistudio <kevin5404@naver.com>
2020-05-04 23:17:15 +0900

    asdfjklasfdjlk

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token;
-
 const welcomeChannelName = "👋어서오세요";
+// const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "(아/야) 새로운 심청이는 언제나 환영이야!";
+// const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-});
-
-client.on('message', (message) => {
-  if(message.content === '심하') {
-    message.reply('심청이 하이욧:)');
-  }
-
-});
-
-client.on('message', (message) => {
-  if(message.content === '심바') {
-    message.reply('심청이 바바루 굿밤>_<');
-  }
 });
 
 client.on("guildMemberAdd", (member) => {
@@ -28,8 +16,28 @@ client.on("guildMemberAdd", (member) => {
   const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "게스트"));
 });
 
-member.addRole(guild.roles.find(role => role.name == "심청영🌸"));
+// client.on("guildMemberRemove", (member) => {
+//   const guild = member.guild;
+//   const deleteUser = member.user;
+//   const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+//   byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+// });
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+
+  if(message.content == '심바') {
+    message.reply('심청이 바바루 굿밤>_<');
+  }
+
+  if(message.content == '심하') {
+    message.reply('심청이 하이욧:)');
+  }
+});
 
 client.login(token);
