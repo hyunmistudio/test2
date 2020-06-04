956d9b3f2f635025bb39fd0a84771327f56e3cf8
hyunmistudio <kevin5404@naver.com>
2020-06-04 23:24:57 +0900

    ㄴㄻㅇㄻㄹㄴ

--- index.js
+++ index.js
@@ -30,6 +30,55 @@ client.on("guildMemberAdd", (member) => {
 // });
 
 client.on("message", async message => {
+
+  if(command === "!킥") {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit: 
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("당신은 관리자가 아닙니다");
+    
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    // We can also support getting the member by ID, which would be args[0]
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("대상자를 맨션해주세요");
+    if(!member.kickable) 
+      return message.reply("너 보다 역할이 높은것 같은데 킥을 못해주겠다 ㅋ");
+    
+    // slice(1) removes the first part, which here should be the user mention or ID
+    // join(' ') takes all the various parts to make it a single string.
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "사유 작성 바람";
+    
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "!ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("넌 관리자가 아니야");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("대상자를 맨션해주세요");
+    if(!member.bannable) 
+      return message.reply("너 보다 역할이 높은것 같은데 킥을 못해주겠다 ㅋ");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "사유 작성 바람";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+
   if(message.author.bot) return
 
   if(message.content.startsWith('!사다리')) {
